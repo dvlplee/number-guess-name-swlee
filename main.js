391f181cd21181a19ce0d6482fca534053fa23f1
Seungwon Lee <dvlplee16@gmail.com>
2024-02-12 00:27:05 +0900

    second commit

--- main.js
+++ main.js
@@ -13,10 +13,19 @@ let computerNum = 0
 let playButton = document.getElementById("play-button") //html에서 가져옴
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
+let resetButton = document.getElementById("reset-button")
+let chances = 5
+let gameOver = false
+let chanceArea = document.getElementById("chance-area")
+let history =[]
 
 playButton.addEventListener("click", play) // 함수도 매개변수로 넘길수있다.
+resetButton.addEventListener("click", reset)
+userInput.addEventListener("focus", function() { // 익명함수
+    userInput.value = ""
+})
 
-console.log(playButton)
+
 function pickRandomNum() {
     computerNum = Math.floor(Math.random() * 100) + 1
     console.log("정답", computerNum)
@@ -24,12 +33,51 @@ function pickRandomNum() {
 
 function play() {
     let userValue =  userInput.value
+
+    if(userValue<1 || userValue>100) {
+        resultArea.textContent = "1과 100사이 숫자를 입력해주세요."
+        return
+    }
+    if(history.includes(userValue)) {
+        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요. "
+        return
+    }
+
+    chances--
+    chanceArea.textContent = `남은 기회: ${chances}번`
+    console.log("chance는", chances)
+
     if(userValue < computerNum) {
         resultArea.textContent = "UP!!!"
     } else if(userValue > computerNum) {
         resultArea.textContent = "DOWN!!!"
     } else {
         resultArea.textContent = "맞췄습니다!!!"
+        gameOver = true
     }
+
+    history.push(userValue)
+    console.log(history)
+
+    if(chances < 1) {
+        gameOver = true
+    }
+
+    if (gameOver == true) {
+        playButton.disabled = true
+    }
+}
+
+function reset() {
+    //user input창 깨끗이 정리
+    userInput.value = ""
+    
+    //새로운 번호 생성
+    pickRandomNum()
+    gameOver = false;
+    playButton.disabled = false;
+    chances = 5;
+    chanceArea.innerHTML = `남은 기회: ${chances}번`;
+    resultArea.textContent = "결과값이 여기 나옵니다!"
 }
 pickRandomNum()
